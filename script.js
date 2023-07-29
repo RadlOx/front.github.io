f44acadcae9a6d588620034de333c9a08910224f
RadlOx <lucabass1@gmail.com>
2023-07-29 15:22:51 +0200

    site

--- script.js
+++ script.js
@@ -1,106 +1,38 @@
-// const input = document.getElementById("input-search");
-// input.addEventListener("keyup", autocomplete)
-// const risultati = document.getElementById("search-resoult");
-// const nomi = [
-// 	"andrea",
-// 	"luca",
-// 	"marco",
-// 	"cristian",
-// 	"antonio",
-// 	"alessio",
-// 	"enrica",
-// 	"martina",
-// 	];
+window.addEventListener('scroll', function() {
+	document.querySelector('box-icon').style.translateX = "(70px)";
 
-// 	function autoCompleteMatch(valore){
-// 		if ( valore == "") return []
-// 		const reg = new RegExp(valore)
-// 		return nomi.filter(nome => {
-// 			if (nome.match(reg)) return nome
-// 		})
-// 	}
+  });
+  
 
-// 	function autocomplete(event){
-// 		let nomiConsigliati = ""
-// 		const nomi = autoCompleteMatch(event.target.value)
-// 		nomi.forEach((nome)=>{
-// 			nomiConsigliati += `<li onclick="seleziona(event)">${nome}</li>`
-// 		})
-// 		risultati.innerHTML = `<ul>${nomiConsigliati}</ul>`
-// 	}
-// 	function seleziona(event) {
-// 		risultati.innerHTML = ""
-// 		input.value = event.target.textContent 
-// 	}
+//* riempimento header	
+	var check = document.getElementById("check-menu");
+	var header = document.querySelector("header");
+	var body = document.querySelector("body");
 
+	function riempHead(){
+		if (check.checked == true) {
+			header.style.transition = "all .25s";
+			header.style.backgroundColor ="#181818";
+			body.style.overflow = "hidden";
+
+		}
+		else {
+			header.style.backgroundColor ="transparent";
+			body.style.overflow = "scroll";
+
+		}
+	}
+	/********/
 
 	/**///// */
 
 	function toggleMute() {
 
 		var video=document.getElementById("videoId1");
-		
-	  
-		if(video.muted){
-		  video.muted = false;
-		} else {
-		  debugger;
-		  video.muted = true;
-		  video.play()
-		}
-	  
-	  }
-	  
-	  $(document).ready(function(){
-		setTimeout(toggleMute,3000);
-	  })
-	  // secondo video
-	  function toggleMute() {
-
 		var video=document.getElementById("videoId2");
-		
-	  
-		if(video.muted){
-		  video.muted = false;
-		} else {
-		  debugger;
-		  video.muted = true;
-		  video.play()
-		}
-	  
-	  }
-	  
-	  $(document).ready(function(){
-		setTimeout(toggleMute,3000);
-	  })
-	  
-	  // terzo video 
-
-	  function toggleMute() {
-
 		var video=document.getElementById("videoId3");
-		
-	  
-		if(video.muted){
-		  video.muted = false;
-		} else {
-		  debugger;
-		  video.muted = true;
-		  video.play()
-		}
-	  
-	  }
-	  
-	  $(document).ready(function(){
-		setTimeout(toggleMute,3000);
-	  })
-
-	  //
-
-	  function toggleMute() {
-
 		var video=document.getElementById("videoId4");
-		
+
 	  
 		if(video.muted){
 		  video.muted = false;
@@ -115,3 +47,4 @@
 	  $(document).ready(function(){
 		setTimeout(toggleMute,3000);
 	  })
+	  
