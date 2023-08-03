cb27d56c146e5bec0d37eecc231741ceec4fe0a3
RadlOx <lucabass1@gmail.com>
2023-08-03 18:18:45 +0200

    d

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-	  
-if(video.muted){
+	// SCRIPT AUTOPLAY VIDEO  
+const video = document.getElementById("slide-main1")
+	if(video.muted){
 	video.muted = false;
 } else {
 	debugger;
 	video.muted = true;
 	video.play()
-  }
-		
-
-		
-  
+}
 
