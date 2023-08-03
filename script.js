d567c342053469fac746171d6fbcb89b561fd592
RadlOx <lucabass1@gmail.com>
2023-08-03 17:50:44 +0200

    si

---
+++ script.js
@@ -0,0 +1,8 @@
+var vid = document.getElementById("slide-main1");
+var vid = document.getElementById("slide-main2");
+var vid = document.getElementById("slide-main3");
+var vid = document.getElementById("slide-main4");
+
+vid.autoplay = true;
+vid.load();
+
