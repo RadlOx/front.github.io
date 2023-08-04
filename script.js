0711879a106e6a0bf68533c40351c3138373df45
RadlOx <lucabass1@gmail.com>
2023-08-04 23:35:16 +0200

    site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 	// SCRIPT AUTOPLAY VIDEO  
-	function toggleMute() {
+function toggleMute() {
 		
-		var video = document.getElementById("slide-main1")
+	var video = document.getElementById("slide-main1")
 
 	if(video.muted){
 	  video.muted = false;
@@ -10,5 +10,32 @@
 	  video.muted = true;
 	  video.play()
 	}
-  
-  }
+} 
+  /////////////////////////
+
+  	//SCRIPT SLIDE MAIN 
+
+var n_slide = 4;
+var corrente = 1;
+
+function succ(){
+	corrente++;
+	if (corrente > n_slide) {
+		corrente=1 ;
+	}
+	for (var i = n_slide; i>0;i--){
+		document.querySelector(".slider-main:nth-child("+ i + ")").style.display = "none";
+	}
+	document.querySelector(".slider-main:nth-child("+ corrente + ")").style.display = "block";
+}
+
+function precc(){
+	corrente--;
+	if (corrente ==0) {
+		corrente= n_slide ;
+	}
+	for (var i = n_slide; i>0;i--){
+		document.querySelector(".slider-main:nth-child("+ i + ")").style.display = "none";
+	}
+	document.querySelector(".slider-main:nth-child("+ corrente + ")").style.display = "block";
+}
