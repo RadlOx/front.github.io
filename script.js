3eaf49aa2988b2c2f5f0fc9091cf611ac3ed9d15
RadlOx <lucabass1@gmail.com>
2023-08-27 17:25:47 +0200

    c

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ var cipolla = document.getElementById ('parallax-img6');
 var paneSopra = document.getElementById ('parallax-img7');
 var scritta = document.getElementById ('parallax-img8');
 var secHamb = document.getElementById ('sec-hamburger');
+var txtIng = document.getElementById('txt-box');
 
 
 window.addEventListener('scroll', () => {
 	let value = window.scrollY;
 
 	if ( value<180) {
-		paneSotto.style.top = value * 2.5 + 'px';
-		lattuga.style.top = value * 2 + 'px';
-		carne.style.top = value * 1.5 + 'px';
-		formaggio.style.top = value * 1.3 + 'px';
+		paneSotto.style.top = value * 2.3 + 'px';
+		lattuga.style.top = value * 2.1 + 'px';
+		carne.style.top = value * 1.7 + 'px';
+		formaggio.style.top = value * 1.4 + 'px';
 		pomodoro.style.top = value * 1 + 'px';
 		cipolla.style.top = value * .7 + 'px';
 	}
@@ -27,14 +28,17 @@ window.addEventListener('scroll', () => {
 	else {
 		scritta.style.opacity = 1;
 	}
-	
+
 	if (value>130) {
 		secHamb.style.transform = "translateX(50%)";
+		txtIng.style.transform = "translateX(0%)"
 		scritta.style.display = "none";
 	}
 	else {
 		secHamb.style.transform = "translateX(0%)";
 		scritta.style.display = "block";
+		txtIng.style.transform = "translateX(-100%)"
+
 
 	}
 })
