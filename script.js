cebd44b42c73d95c4031bb45ecec0f50421ac623
RadlOx <lucabass1@gmail.com>
2023-08-30 15:09:53 +0200

    d

--- script.js
+++ script.js
@@ -11,31 +11,35 @@ var scritta = document.getElementById('txt-png');
 var box = document.getElementById('parallax-container');
 var circle1 = document.getElementById('circle-1');
 var circle2 = document.getElementById('circle-2');
+var circleKcal = document.getElementById('circle-kcal');
+var contKcal = document.getElementById('cont-kcal');
 var ingr = document.getElementById('ingredienti');
 
 window.addEventListener('scroll', () => {
 	var value = window.scrollY;
 
-	if (value<60) {
+	if (value<50) {
 		paneSotto.style.top = value * 6 + 'px';
 		lattuga.style.top = value *  5 + 'px';
 		carne.style.top = value *  4 + 'px';
 		formaggio.style.top = value *  3 + 'px';
 		pomodoro.style.top = value *  2 + 'px';
-		cipolla.style.top = value *  1 + 'px';
+		cipolla.style.top = value *  .8 + 'px';
 		paneSopra.style.top = value *  -1 + 'px';
 	}
 
-	if (value>58) {
+	if (value>48) {
 		circle1.style.top = '25%';
 		circle1.style.transform = 'scale(1.7)';
 		circle2.style.transform = 'scale(1.7)';
 	}
-	if (value>60) {
+	if (value>50) {
 		box.style.transform = "translateX(50%)";
-		scritta.style.transform = "translateX(-67%) translateY(-35%)";
-		scritta.style.width = '80%';
-		ingr.style.transform = 'translateX(0%)';
+		scritta.style.transform = "translateX(-67%) translateY(-25%)";
+		scritta.style.width = '60%';
+		ingr.style.transform = 'translateX(0%) translateY(-5%)';
+		circleKcal.style.transform = 'translateX(10%) translateY(99%) scale(.8)';
+		contKcal.style.opacity = '1'
 	}
 	else {
 		scritta.style.transform = "translateY(0%)";
@@ -45,6 +49,8 @@ window.addEventListener('scroll', () => {
 		circle2.style.transform = 'translateX(-30%)';
 		circle1.style.transform = 'scale(1)';
 		ingr.style.transform = 'translateX(-100%)';
+		circleKcal.style.transform = 'translateX(0%) translateY(0%) scale(1)';
+		contKcal.style.opacity = '0'
 	}
 })
 
